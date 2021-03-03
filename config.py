f8613de171cc6f577a85f64de024eea6335b21d8
DRAJ <72666713+hyy-iam-raj@users.noreply.github.com>
2021-03-03 13:21:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+from sample_config import Config
+
+
+class Development(Config):
+  # REQUIRED
+    API_ID_KEY = "1979859"
+    API_HASH_KEY = "2d8efd791d4ae887c2411aee30f3542e"  
+    STRING_SESSION = "1BVtsOH4Bu5jFv2VgCGc7AnmTg4wlGH59fR_LTZx7yXYxZzrcUi1P-J-fqr5b6wFrwE4J7ChrfWmVPML0CAv1octtTfi5eIJQ_jfB-eV5vnOUIyU3GIAKLDw1JRpkjEcHc8A3sfLlO8CK-VADHRGsmk8XewBmKiL2B24ibF_PCzxxqIZjHndkR9505UZY3cQa9CRaXnDfmjGCZ4AiJuu0kZk7T2OWqqezWgKji0tCVugV0fOjYYJZFwON8BOU-ZFk0uJMvwWbiV7zvJ_o_kg33zNuWL3R_WghXDcwmNdvYaEV3wlT1TF5-oqW6Ert5y7nItgZc1pUWYvXr_RFsImNh6Zdkqso5ho="
+    SIBYL = "1634756943"
+    ENFORCERS = "1634756943"
+    Sibyl_logs = "-562982633"
+    Sibyl_approved_logs = "-562982633"
+    GBAN_MSG_LOGS = "-562982633"
