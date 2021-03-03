22647098561f6766c51758fe92b1c5a2f6b68187
DRAJ <72666713+hyy-iam-raj@users.noreply.github.com>
2021-03-03 14:10:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import Sibyl_System.config as Config
 
 class Development(Config):
   # REQUIRED
-    API_ID_KEY = 1979859
+    API_ID_KEY = "1979859"
     API_HASH_KEY = "2d8efd791d4ae887c2411aee30f3542e"  
     STRING_SESSION = "1BVtsOH4Bu5jFv2VgCGc7AnmTg4wlGH59fR_LTZx7yXYxZzrcUi1P-J-fqr5b6wFrwE4J7ChrfWmVPML0CAv1octtTfi5eIJQ_jfB-eV5vnOUIyU3GIAKLDw1JRpkjEcHc8A3sfLlO8CK-VADHRGsmk8XewBmKiL2B24ibF_PCzxxqIZjHndkR9505UZY3cQa9CRaXnDfmjGCZ4AiJuu0kZk7T2OWqqezWgKji0tCVugV0fOjYYJZFwON8BOU-ZFk0uJMvwWbiV7zvJ_o_kg33zNuWL3R_WghXDcwmNdvYaEV3wlT1TF5-oqW6Ert5y7nItgZc1pUWYvXr_RFsImNh6Zdkqso5ho="
     SIBYL = "1634756943"
